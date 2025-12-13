91ae99ffeeb77323f86432322664aac1aca55ad4
David <davidss231006@gmail.com>
2025-12-12 20:01:09 -0500

    cf

--- CodeForces/459A.cpp
+++ CodeForces/459A.cpp
@@ -11,7 +11,29 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(nullptr);
 
+    long long x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+    cin >> x1 >> y1 >> x2 >> y2;
 
+
+    if(x1 != x2 && y1 != y2 && abs(x1 - x2) != abs(y1 - y2)) {
+
+        cout << - 1 << "\n";
+
+    } else if(y1 == y2) {
+
+      cout << x1 << " " << y1 + abs(x1 - x2) << " " << x2 << " " << y2 + abs(x1 - x2) << "\n";
+
+    } else if(x1 == x2) {
+
+        cout << x1  + abs(y1 - y2) << " " << y1 << " " << x2 + abs(y1 - y2) << " " << y2  << "\n";
+
+
+    } else {
+
+        cout << x1 << " " << y2 << " " << x2 << " " << y1 << "\n";
+
+
+    }
 
 
 
