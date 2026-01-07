b84c88d4bd0cb39eadcddfec5077450eb778b0b3
David <davidss231006@gmail.com>
2026-01-07 10:01:09 -0500

    cf

---
+++ CodeForces/2183A.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define debug(x) cout<<#x<<": "<<x<<"\n"
+#define all(x) (x).begin(), x.end()
+#define rall(x) (x).rbegin(), x.rend()
+#define INF -1
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int t = 0;
+    cin >> t;
+
+    while(t-- > 0) {
+
+    int n = 0;
+    cin >> n;
+
+    vector<int> a(n);
+
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+
+    if(a[0] == 1 || a[n - 1] == 1) cout << "Alice\n";
+    else cout << "Bob\n";
+
+    }
+
+
+
+
+    return 0;
+}
+
+
+
