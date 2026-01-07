964c7bef0c68e42afe4343e788beba03f62f52b9
David <davidss231006@gmail.com>
2026-01-07 07:12:15 -0500

    cf

---
+++ CodeForces/1373A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define debug(x) cout<<#x<<": "<<x<<"\n"
+#define all(x) (x).begin(), x.end()
+#define rall(x) (x).rbegin(), x.rend()
+#define INF -1
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int t = 0;
+    cin >> t;
+
+    while(t-->0) {
+
+        long long a = 0, b = 0, c = 0;
+        cin >> a >> b >> c;
+
+        cout << (a < c ? 1 : -1) << " " << (c < a * b ? b : - 1) << "\n";
+
+
+    }
+
+
+
+
+    return 0;
+}
+
+
