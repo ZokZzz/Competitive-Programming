4e641351a75f6771082411746b6d2265f690f8c4
David <davidss231006@gmail.com>
2026-01-31 22:04:56 -0500

    cf

---
+++ CodeForces/2103A.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define debug(x) cout<<#x<<": "<<x<<"\n"
+#define all(x) (x).begin(), x.end()
+#define rall(x) (x).rbegin(), x.rend()
+#define INF -1
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int t = 0;
+    cin >> t;
+
+    while(t-->0){
+
+        int n = 0;
+        cin >> n;
+
+        vector<int> a(n);
+        set<int> s;
+
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+            s.insert(a[i]);
+
+        }
+
+        cout << s.size() << "\n";
+
+
+    }
+
+
+
+
+    return 0;
+}
+
