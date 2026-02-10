f22673f1c599bc69948f216db9a022db87533235
David <davidss231006@gmail.com>
2026-02-10 11:23:42 -0500

    bc

--- BeeCrowd/1248.cpp
+++ BeeCrowd/1248.cpp
@@ -1,80 +1,76 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define pb push_back
+#define debug(x) cout<<#x<<": "<<x<<"\n"
+#define all(x) (x).begin(), x.end()
+#define rall(x) (x).rbegin(), x.rend()
+#define INF -1
 
 
-int main(){
-ios_base::sync_with_stdio(0);
-cin.tie(0);
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
 
-int n;
-cin>>n;
-cin.ignore();
-for(int sorner=0; sorner<n; sorner++){
-string dieta, c1, c2;
-getline(cin, dieta); getline(cin, c1); getline(cin, c2);
-if(dieta.empty()){
-cout<<"\n";
-continue;
+
+    int t = 0;
+    cin >> t;
+
+    cin.ignore();
+
+    while(t-- > 0){
+
+        string dieta;
+        getline(cin, dieta);
+
+        string d, a;
+        getline(cin, d);
+        getline(cin, a);
+
+
+        string comida = d + a;
+
+        vector<int> f (26, 0);
+
+        for(char c : dieta) f[c - 'A']++;
+
+        bool cheat = false;
+
+        for(char c : d + a){
+
+            int x = c - 'A';
+
+            if(f[x] == 0){
+
+                cheat = true;
+
+            }
+
+            f[x]--;
+
+        }
+
+        if(cheat) {
+
+            cout << "CHEATER\n";
+            continue;
+
+        }
+
+        string ans = "";
+
+        for(int i = 0; i < 26; i++) ans += string(f[i], char(i + 'A'));
+
+        cout << ans << "\n";
+
+
+
+
+    }
+
+
+
+
+    return 0;
 }
 
-if(dieta.empty() && c1.empty() && c2.empty()){
-cout<<"\n";
-continue;
-}
-
-
-if(c1.empty() && c2.empty()){
-sort(dieta.begin(), dieta.end());
-cout<<dieta<<"\n";
-continue;
-}
-
-
-string c = c1+c2;
-set<char>d;
-set<char>co;
-for(int i=0; i<c.size(); i++){
-  co.insert(c[i]);
-}//for
-
-if(c.size()!=co.size()){
-cout<<"CHEATER\n";
-continue;
-}//if
-
-for(int i=0; i<dieta.size(); i++){
-  d.insert(dieta[i]);
-}//for
-
-vector<char>di;
-di.reserve(d.size());
-copy(d.begin(), d.end(), back_inserter(di));
-
-
-
-for(int i=0; i<c.size(); i++){
-   di.erase(remove(di.begin(), di.end(), c[i]), di.end());
-}//for
-
-bool f=true;
-
-for(char x : c){
-if(find(dieta.begin(), dieta.end(), x) == dieta.end()){
-f=false;
-break;
-}//if
-}//for
-
-
-if(f){
-for(int i=0; i<di.size(); i++){
-cout<<di[i];
-}//for
-cout<<"\n";
-}else{
-cout<<"CHEATER\n";
-}//if
-}//for
-
-return 0;
-}
---
+++ CodeForces/2194C.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define debug(x) cout<<#x<<": "<<x<<"\n"
+#define all(x) (x).begin(), x.end()
+#define rall(x) (x).rbegin(), x.rend()
+#define INF -1
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int t = 0;
+    cin >> t;
+
+    while(t-->0){
+
+
+        int n = 0, k = 0;
+        cin >> n >> k;
+
+        vector<string> a (k);
+
+        for(int i = 0; i < k; i++) cin >> a[i];
+
+
+
+
+
+    }
+
+
+
+
+    return 0;
+}
+
