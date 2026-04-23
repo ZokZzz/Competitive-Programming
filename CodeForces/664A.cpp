61fec5f217d1c0f53a6d86fbb4bc9372ddadec9a
David <davidss231006@gmail.com>
2026-04-23 16:08:21 -0500

    cf

---
+++ CodeForces/664A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define debug(x) cout<<#x<<": "<<x<<"\n"
+#define all(x) (x).begin(), x.end()
+#define rall(x) (x).rbegin(), x.rend()
+#define INF -1
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    string a, b;
+    cin >> a >> b;
+
+    cout << (a == b ? a : "1") << "\n";
+
+    return 0;
+}
+
+
+
