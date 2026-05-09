bcbbdac9d8dfea28a1d86464c3a53389e703b9e3
David <davidss231006@gmail.com>
2026-05-09 13:12:15 -0500

    cf

---
+++ CodeForces/2210A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define debug(x) cout<<#x<<": "<<x<<"\n"
+#define all(x) (x).begin(), x.end()
+#define rall(x) (x).rbegin(), x.rend()
+#define INF -1
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define sz(a) ((int)a.size())
+
+
+void tc(){
+
+    long long n;
+    cin >> n;
+
+    for(int i = n; i >= 1; i--) cout << i << (i == 1? "\n" : " ");
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--){
+        tc();
+    }
+}
+
+
