e69e9ed2a3e122cd880e2e7bde4602a10f577ab4
David <davidss231006@gmail.com>
2026-07-02 13:52:21 -0500

    cf

---
+++ CodeForces/2241A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define debug(x) cout<<#x<<": "<<x<<"\n"
+#define all(x) (x).begin(), x.end()
+#define rall(x) (x).rbegin(), x.rend()
+#define INF -1
+#define vi vector<int>
+#define vll vector<long long>
+#define vvi vector<vector<int>>
+#define sz(a) ((int)a.size())
+#define full(x, y) memset(x, y, sizeof(x))
+
+
+void tc(){
+
+    int x = 0, y = 0;
+    cin >> x >> y;
+
+    if(x % y == 0) cout << "YES\n";
+    else cout << "NO\n";
+
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t-->0){
+        tc();
+    }
+}
+
