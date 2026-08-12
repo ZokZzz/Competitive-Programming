13e231ee7fe2303b602b4c5d6b99a49d97abf61b
David <davidss231006@gmail.com>
2026-08-12 12:09:09 -0500

    t

---
+++ CodeForces/2252A.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define debug(x) cout<<#x<<": "<<x<<"\n"
+#define all(x) (x).begin(), x.end()
+#define rall(x) (x).rbegin(), x.rend()
+#define oo 1e9
+#define vi vector<int>
+#define vll vector<long long>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<long long>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define si set<int>
+#define sll set<long long>
+#define sti stack<int>
+#define stll stack<long long>
+#define stc stack<char>
+#define mii map<int, int>
+#define mll map<long long, long long>
+#define qi queue<int>
+#define qll queue<long long>
+#define qc queue<char>
+#define pqi priority_queue<int>
+#define pqll priority_queue<long long>
+#define pqpii priority_queue<pair<int, int>>
+#define pqpll priority_queue<pair<long long, long long>>
+#define pqim priority_queue<int, vector<int>, greater<int>>
+#define pqllm priority_queue<long long, vector<long long>, greater<long long>>
+#define pqpiim priority_queue<pii, vector<pii>, greater<pii>>
+#define pqpllm priority_queue<pll, vector<pll>, greater<pll>>
+#define sz(a) ((int)a.size())
+#define memfull(x, y) memset(x, y, sizeof(x))
+
+
+void tc(){
+
+    int n = 0;
+    cin >> n;
+
+    vi a (n);
+    mii f;
+    for(int i = 0; i < n; i++){
+
+        cin >> a[i];
+        f[a[i]]++;
+
+    }
+
+    if(f.size() == 1){
+
+        for(auto i : f){
+
+            if(i.second == 1){
+
+                cout << i.first <<"\n";
+                return;
+
+            } else {
+
+                cout << i.first + i.first <<"\n";
+                return;
+
+            }
+
+        }
+
+
+    }
+
+    int hf = 0, M = 0;
+
+    for(auto i : f){
+
+        if(i.second > hf){
+
+            hf = i.second;
+            M = i.first;
+
+        }
+
+    }
+
+    int rest = n - hf;
+
+    int resthf = hf - rest;
+
+    f[M] -= resthf;
+
+    resthf = min(2, resthf);
+
+    int ans = 0;
+
+    for(auto i : f) ans += i.first * i.second;
+
+    ans += M * resthf;
+
+    cout << ans << "\n";
+
+  
+    
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t-->0){
+        tc();
+    }
+}
